dbef170dffa608ad213ccac2f00d1f52f73b9b32
Aribeljdr <aribeljdr@gmail.com>
2024-04-13 22:11:31 -0500

    Leer los datos del curso que seleccionamos

--- ShoppingCart/js/app.js
+++ ShoppingCart/js/app.js
@@ -3,7 +3,7 @@ const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
-
+ 
 cargarEventListeners();
 function cargarEventListeners (){
     //Cuando agregas un curso  "Agregar al carrito"
@@ -13,6 +13,21 @@ function cargarEventListeners (){
 function agregarCurso(e){
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')){
-        console.log('Agregando al carrito...');
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
     }
 }
+//Lee el contenido del HTML  y estrae la informacion del curso
+function leerDatosCurso(curso){
+    // console.log(curso);
+
+    //Crear un objeto con eñ contenido del cursos actual 
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoCurso);
+};
