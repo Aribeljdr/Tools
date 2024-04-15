9c6d319abd7243db7892ace65f968cfb87e5b9bc
Aribeljdr <aribeljdr@gmail.com>
2024-04-14 19:34:44 -0500

    Validando Email

--- SendingEmails/js/app.js
+++ SendingEmails/js/app.js
@@ -14,23 +14,37 @@ document.addEventListener('DOMContentLoaded',function(){
         if(e.target.value.trim() === ''){
             // console.log('Esta vacio');
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`,e.target.parentElement);
-        }else{
-            console.log('Si hay algo ...');
+            return;
         }
+
+        if(e.target.id === 'email' && !validarEmail(e.target.value)){
+            mostrarAlerta('El email no es valido', e.target.parentElement);
+            return;
+        }
+
+        limpiarAlerta(e.target.parentElement);
     }
 
     function mostrarAlerta(mensaje,referencia){
-        //Comprueba si ya existe uan alerta
-        const alerta = referencia.querySelector('.bg-red-600');
-        if(alerta){
-            alerta.remove();
-        }
-
+        
+        limpiarAlerta(referencia);
         //Generar alerta en HTML
         const error = document.createElement('P');
         error.textContent = mensaje;
         error.classList.add('bg-red-600','text-white','p-2','text-center')
         //inyectar el error al formulario
         referencia.appendChild(error);
+    }
+    function limpiarAlerta(referencia){
+        //Comprueba si ya existe uan alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta){
+            alerta.remove();
+        }
+    }
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const resultado = regex.test(email);
+        return resultado;
     }
 })
